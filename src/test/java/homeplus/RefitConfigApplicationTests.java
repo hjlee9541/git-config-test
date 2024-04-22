ec8e9047a29fbb66e3d5e43e62390aa48aeaf462
이호준 <hojoon.lee@homeplus.co.kr>
2024-04-22 13:35:13 +0900

    first commit

---
+++ src/test/java/homeplus/RefitConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package homeplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefitConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
